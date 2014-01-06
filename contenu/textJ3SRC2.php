baf3589b701cba54705e2c6c1fc17163e8fb950e
Claire Dune <claire.dune@gmail.com>
2014-01-06 12:54:22 +0100

    changement soutenance SRC2

--- contenu/textJ3SRC2.php
+++ contenu/textJ3SRC2.php
@@ -2,15 +2,14 @@
  				
 		<div class ="alert alert-info">
 			Le jalon 3 est une soutenance dont la date est fixée. C'est une VALIDATION PILOTE.
-			Suite à la soutenance, un rapport sera déposé sur le gware listant des recommandations
-			à prendre en compte dans la suite du projet.
+			Suite à la soutenance, une liste de recommandations vous sera remise. Vous devrez en tenir compte dans la suite de votre projet.
 	</div>
 
  		
  				
  						<H3>1. Objectif </H3>
  						<p>
- 							A mi parcours des projets, vous présenterez votre avancement sous la forme d'une soutenance technique.
+ 							A mi parcours des projets, vous présenterez votre avancement.
  							L'objectif est de faire un point et de dialoguer avec le jury afin de modifier le plan de travail des
  							mois à venir si nécessaire.
  						</p>				
@@ -21,15 +20,18 @@
  						
  						<H3>2. Formalisme</H3>
  						
+ 						
  						<p>
- 							Elle prendront soit la forme d'une soutenance unique avec des enseignants de tous les domaines présents, soit la forme d'une série de petites soutenances portant chacune sur un domaine bien spécifique avec deux enseignants spécialiste. 
+ 							Elle prendront soit la forme d'un marathon de soutenances, c'est à dire une série de petites soutenances portant chacune sur un domaine bien spécifique avec des jurys composés d'enseignants spécialistes du domaine. 
  						</p>
  						
  						<div class ="alert alert-info"> 
  							La forme de la présentation est libre. Dans ces soutenances, il sera laisser une grande part à la discussion.  
  						</div>
  						
- 						 						
+ 						<div class ="alert alert-info"> 
+ 							Chaque soutenance dure MAXIMUM 5' et est suivie de 15 à 20 minutes de question. Toutes les 30', un nouveau groupe entre dans la salle.  
+ 						</div>				
  						<p>
  							 							
 							Vous pourrez vous appuyer sur des transparents conformes
@@ -42,44 +44,19 @@
  							durant les questions, tous les membres de l'agence pourront être intérrogés.
  						</p>
  						
+ 						<p>
+ 						 Les étudiants présentent sur leur ordinateur personnel. Au besoin il peuvent demander un ordinateur au service technique du département au minimum 15 jours avant la date de la soutenance.
+ 						</p>
+ 						
  						<H3>3. Contenu </H3>
 
-						<ul>
-						<li><H4>Fiche projet</H4> 
-						amener une version imprimée de votre fiche projet pour les membres du jury.</li>
+						<p>Les étudiants doivent se présenter avec toutes leurs réalisations ainsi qu'une page A4 présentant l'avancée de leurs travaux dans la matière.</p>
+						<div class ="alert alert-info">
+						Toute absence d'une document demandé par le jury peut mettre fin à la soutenance.
+						</div>
 						
-						<li> <H4>Etat d'avancement du projet</H4> 
-					       <ul>
-					       <li><b>informatique</b> : présentation de la base de données et du développement web minimum démandé 
-					       pour chaque projet</li>
-					       <li><b>web design & video</b> : présentation du design graphique, prouver que le template est 
-					       <i>maison</i>, validation 
-					       de la partie ergonomie par envoie de l'URL 1 semaine avant la soutenance. 
-					       & <b>si vidéo</b> : état d’avancement (recrutement des acteurs, avancement du scénario, outillage)</li>
-						 	<li><b>marketing & communication </b> : analyse de marché & concurrence, stratégie marketing, 
-						 	plan de communication, ligne éditoriale</li>
-						 	</ul>
-						 </li>
-						 <li> <H4>Questions / réponses</H4> 
-						 <ul>
-						 <li>
-						   Gestion de projet ;
-						 </li> 
-						 <li>
-						 	Retour sur des points techniques (prévoyez vos codes et vos réalisations) ;
-						 </li>
-						 <li>
-						 	Etablissement d'une liste de modifications à apporter au projet, si nécessaire. Toutes les modifications
-						 	demandées devront être appliquées et visible lors de la soutenance finale.
-						 </li>
-						 </ul>
-						 </ul>
-
-					 	<H3>4. Notation </H3>
+					 	<H3>4. Evaluation </H3>
 				
-<br>
-	<a class="btn btn-small" href="files/SRC2GrilleSoutenanceTechnique.doc">télécharger la grille de notation</a>
-<br>
 				
  	<p>
  		Un rapport est délivré par le jury à l'issu de la soutenance. Il contient une liste de recommandations qui 
@@ -87,4 +64,7 @@
  		Ce rapport sera déposé sur le gware par le jury suite à la soutenance.
  	</p>
 
+<p>
+ En plus de chaque rapport, chaque groupe reçoit une note sur 5 points.
+</p>
 
